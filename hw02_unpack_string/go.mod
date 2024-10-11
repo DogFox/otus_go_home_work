a51570230763e2765825e23f28ea99ade2fa155e
Denis Kurov <kurovda@gmail.com>
2024-10-11 20:19:43 +0300

    hw02_unpack_string completed

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/DogFox/otus_go_home_work/hw01_hello_otus
 
 go 1.22
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,68 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	lenQueue := utf8.RuneCountInString(str)
+	queue := make([]string, lenQueue)
+	for i, v := range str {
+		queue[i] = string(v)
+	}
+
+	result := ""
+	shield := false
+
+	for len(queue) > 0 {
+		first := queue[0] // Первый элемент очереди
+		second := "1"
+		if len(queue) > 1 {
+			second = queue[1]
+		}
+
+		// fmt.Println(first, " ", second, " len ", len(queue))
+		// низя чтобы сразу чиселка была
+		_, err := strconv.Atoi(first)
+		if err == nil {
+			if !shield {
+				return "", ErrInvalidString
+			}
+		}
+
+		if first == "\\" && !shield {
+			// флаг поднять, скипаем
+			shield = true
+			queue = queue[1:]
+			// fmt.Println("shield", shield)
+			continue
+		}
+
+		times, err := strconv.Atoi(second)
+		if err != nil {
+			if shield && second != "n" && second != "\\" {
+				return "", ErrInvalidString
+			}
+			// значит тут символ а не цифра, плюсуем и двигаем дальше
+			result += first
+			queue = queue[1:]
+			shield = false
+
+			continue
+		}
+		result += strings.Repeat(first, times)
+		// fmt.Println(result)
+
+		// Переопределяем очередь сразу на 2 элемента, но если последний, то так и быть...
+		if len(queue) >= 2 {
+			queue = queue[2:]
+		} else {
+			queue = queue[1:]
+		}
+	}
+	return result, nil
 }
