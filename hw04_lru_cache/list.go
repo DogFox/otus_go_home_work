3acf0340e0a72be33baf3e849eb46932bbfc51c4
Denis Kurov <kurovda@gmail.com>
2024-10-29 19:08:28 +0300

    list relization

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,24 +1,87 @@
 package hw04lrucache
 
 type List interface {
-	Len() int
-	Front() *ListItem
-	Back() *ListItem
-	PushFront(v interface{}) *ListItem
-	PushBack(v interface{}) *ListItem
-	Remove(i *ListItem)
-	MoveToFront(i *ListItem)
+	Len() int                          // длина списка
+	Front() *ListItem                  // первый элемент списка
+	Back() *ListItem                   // последний элемент списка
+	PushFront(v interface{}) *ListItem // добавить значение в начало
+	PushBack(v interface{}) *ListItem  // добавить значение в конец
+	Remove(i *ListItem)                // удалить элемент
+	MoveToFront(i *ListItem)           // переместить элемент в начало
 }
 
 type ListItem struct {
-	Value interface{}
-	Next  *ListItem
-	Prev  *ListItem
+	Value interface{} // значение
+	Next  *ListItem   // следующий элемент
+	Prev  *ListItem   // предыдущий элемент
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	// items []ListItem
+	len   int
+	first *ListItem
+	last  *ListItem
+}
+
+func (list *list) Len() int {
+	return list.len //len(list.items)
+}
+func (list *list) IncreaseCount() {
+	list.len++
+}
+func (list *list) DecreaseCount() {
+	list.len--
+}
+func (list *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  list.first,
+		Prev:  nil,
+	}
+	if list.len == 0 {
+		list.first = newItem
+		list.last = newItem
+	}
+	list.IncreaseCount()
+	list.first = newItem
+	// list.items = append(list.items, *newItem)
+
+	return newItem
+}
+func (list *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  list.last,
+	}
+	list.IncreaseCount()
+	list.last = newItem
+	// list.items = append(list.items, *newItem)
+
+	return newItem
+}
+
+func (list *list) Front() *ListItem {
+	return list.first
+}
+
+func (list *list) Back() *ListItem {
+	return list.last
+}
+
+// удалить элемент
+func (list *list) Remove(deleted *ListItem) {
+	deleted.Prev.Next = deleted.Next
+	deleted.Next.Prev = deleted.Prev
+	list.DecreaseCount()
+}
+
+// переместить элемент в начало
+func (list *list) MoveToFront(moved *ListItem) {
+	moved.Prev.Next = moved.Next
+	moved.Next.Prev = moved.Prev
+	list.first.Prev = moved
+	moved.Prev = nil
 }
 
 func NewList() List {
