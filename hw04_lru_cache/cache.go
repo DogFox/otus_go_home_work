aee787083b04d7cc86a334acca0bf624efe02587
Denis Kurov <kurovda@gmail.com>
2024-10-29 22:35:49 +0300

    cache completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,6 +2,11 @@ package hw04lrucache
 
 type Key string
 
+type cacheItem struct {
+	key   Key
+	value interface{}
+}
+
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
@@ -9,11 +14,56 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	// ключ может быть, тогда обновим
+
+	newCacheItem := cacheItem{key: key, value: value}
+	val, ok := cache.items[key]
+	if ok {
+		val.Value = newCacheItem
+		cache.queue.MoveToFront(val)
+	} else {
+		if cache.queue.Len() == cache.capacity {
+			lastItem := cache.queue.Back()
+
+			typedItem, ok := lastItem.Value.(cacheItem)
+			if ok {
+				delete(cache.items, typedItem.key)
+				cache.queue.Remove(lastItem)
+			}
+			return false
+		}
+		newItem := cache.queue.PushFront(newCacheItem)
+		cache.items[key] = newItem
+	}
+	return ok
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	val, ok := cache.items[key]
+	if ok {
+		cache.queue.MoveToFront(val)
+		typedItem, ok := val.Value.(cacheItem)
+		if ok {
+			cache.queue.PushFront(val)
+			cache.queue.Remove(val)
+			return typedItem.value, ok
+		}
+		return nil, false
+	} else {
+		return nil, false
+	}
+}
+
+func (cache *lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items =
+		make(map[Key]*ListItem, cache.capacity)
 }
 
 func NewCache(capacity int) Cache {
