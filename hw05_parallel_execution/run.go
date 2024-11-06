7e29ef992b073d5391c1b313d6f4006c5d341b2c
Denis Kurov <kurovda@gmail.com>
2024-11-06 21:11:55 +0300

    add pool worker

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,12 @@
-package hw05parallelexecution
+package main
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -9,7 +14,73 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
+func Run(tasks []Task, workersCount, maxErrorsCount int) error {
+	/// канал с тасками
+	inputCh := make(chan Task)
+	// канал для сбора результатов
+	outputCh := make(chan error)
+
+	// вейтгруп для ожидания рутинок
+	wg := &sync.WaitGroup{}
+
+	//слайс результатов
+	output := make([]error, 0, len(tasks))
+
+	///
+	go func() {
+		defer close(inputCh)
+
+		for i := range tasks {
+			inputCh <- tasks[i]
+		}
+	}()
+
+	go func() {
+		for i := 0; i < workersCount; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for task := range inputCh {
+					res := task()
+					if (res) != nil {
+						outputCh <- task()
+					}
+				}
+			}()
+
+		}
+		wg.Wait()
+		close(outputCh)
+	}()
+
+	// собираем результаты
+	for res := range outputCh {
+		output = append(output, res)
+	}
+
+	fmt.Println(output)
+
 	// Place your code here.
 	return nil
+}
+
+func main() {
+	tasksCount := 50
+	tasks := make([]Task, 0, tasksCount)
+
+	var runTasksCount int32
+
+	for i := 0; i < tasksCount; i++ {
+		err := fmt.Errorf("error from task %d", i)
+		tasks = append(tasks, func() error {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+			atomic.AddInt32(&runTasksCount, 1)
+			return err
+		})
+	}
+
+	workersCount := 10
+	maxErrorsCount := 23
+	Run(tasks, workersCount, maxErrorsCount)
+
 }
