f7a0fd769e940735968392521f417411e3663bd7
Denis Kurov <kurovda@gmail.com>
2024-11-07 10:01:43 +0300

    deadlock

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -13,61 +13,53 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func worker(wg *sync.WaitGroup, input <-chan Task, output chan<- error, quit chan bool) {
+	defer wg.Done()
+
+	select {
+	case <-quit:
+		close(output)
+		return
+	default:
+		for task := range input {
+			if res := task(); res != nil {
+				output <- res
+				fmt.Println(len(output))
+			}
+		}
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, workersCount, maxErrorsCount int) error {
 	/// канал с тасками
-	inputCh := make(chan Task)
+	inputCh := make(chan Task, len(tasks))
 	// канал для сбора результатов
 	outputCh := make(chan error, maxErrorsCount)
 	quit := make(chan bool)
 	// вейтгруп для ожидания рутинок
 	wg := &sync.WaitGroup{}
 	// m := &sync.Mutex{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		defer close(inputCh)
 
-		for i := range tasks {
-			inputCh <- tasks[i]
+	for i := range tasks {
+		inputCh <- tasks[i]
+	}
+	close(inputCh)
+
+	for i := 0; i < workersCount; i++ {
+		wg.Add(1)
+		go worker(wg, inputCh, outputCh, quit)
+	}
+
+	output := 0
+	for _ = range outputCh {
+		output++
+		if output >= maxErrorsCount {
+			quit <- true
 		}
-	}()
+	}
+	fmt.Println(output)
 
-	go func() {
-		for i := 0; i < workersCount; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				select {
-				case <-quit:
-					return
-				default:
-					for task := range inputCh {
-						if res := task(); res != nil {
-							outputCh <- res
-							fmt.Println(len(outputCh))
-						}
-					}
-				}
-			}()
-		}
-		wg.Wait()
-		close(outputCh)
-	}()
-
-	// собираем результаты
-	//слайс результатов
-	// output := make([]error, 0, len(tasks))
-	// for res := range outputCh {
-	// 	output = append(output, res)
-	// 	// if len(output) >= maxErrorsCount {
-	// 	// 	quit <- true
-	// 	// }
-	// }
-
-	// fmt.Println(output)
-
-	// Place your code here.
 	wg.Wait()
 
 	return nil
