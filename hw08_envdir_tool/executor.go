b7664e7f1349f6d3a4a5a43e8bcd5845d48d18b4
Denis Kurov <kurovda@gmail.com>
2024-12-04 22:09:42 +0300

    clean up

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -7,8 +7,7 @@ import (
 
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmdList []string, env Environment) (returnCode int) {
-	// fmt.Println(env)
-	envForCmd := make([]string, 0, 0)
+	envForCmd := make([]string, 0)
 	for key, value := range env {
 		if value.NeedRemove {
 			if err := os.Unsetenv(key); err != nil {
@@ -23,15 +22,7 @@ func RunCmd(cmdList []string, env Environment) (returnCode int) {
 	}
 	envForCmd = append(envForCmd, os.Environ()...)
 
-	// varName := "FOO"
-	// varValue := os.Getenv(varName)
-	// fmt.Printf("%s=%s\n", varName, varValue)
-	// varName = "BAR"
-	// varValue = os.Getenv(varName)
-	// fmt.Printf("%s=%s\n", varName, varValue)
-
 	cmd := exec.Command(cmdList[0], cmdList[1:]...)
-	// cmd.Env = []string{} // не передаем какие либо енвы os.Ennviron
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
