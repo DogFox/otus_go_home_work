532edcde2eb74a49d98b4648fb2529522af98a07
Denis Kurov <kurovda@gmail.com>
2024-12-04 22:05:51 +0300

    add envs from origin

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -9,7 +8,7 @@ import (
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmdList []string, env Environment) (returnCode int) {
 	// fmt.Println(env)
-
+	envForCmd := make([]string, 0, 0)
 	for key, value := range env {
 		if value.NeedRemove {
 			if err := os.Unsetenv(key); err != nil {
@@ -17,24 +16,27 @@ func RunCmd(cmdList []string, env Environment) (returnCode int) {
 			}
 			continue
 		}
+		envForCmd = append(envForCmd, key+"="+value.Value)
 		if err := os.Setenv(key, value.Value); err != nil {
 			return 1
 		}
 	}
+	envForCmd = append(envForCmd, os.Environ()...)
 
-	varName := "FOO"
-	varValue := os.Getenv(varName)
-	fmt.Printf("%s=%s\n", varName, varValue)
-	varName = "BAR"
-	varValue = os.Getenv(varName)
-	fmt.Printf("%s=%s\n", varName, varValue)
+	// varName := "FOO"
+	// varValue := os.Getenv(varName)
+	// fmt.Printf("%s=%s\n", varName, varValue)
+	// varName = "BAR"
+	// varValue = os.Getenv(varName)
+	// fmt.Printf("%s=%s\n", varName, varValue)
 
 	cmd := exec.Command(cmdList[0], cmdList[1:]...)
-	cmd.Env = []string{} // не передаем какие либо енвы os.Ennviron
+	// cmd.Env = []string{} // не передаем какие либо енвы os.Ennviron
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = envForCmd
 
 	if err := cmd.Run(); err != nil {
 		return 1
