8fa6d854f9a676695b9485aceb850dcf94d2c1d0
Denis Kurov <kurovda@gmail.com>
2024-12-30 11:30:24 +0300

    hw09

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -1,9 +1,11 @@
-package hw09structvalidator
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -13,15 +15,15 @@ type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
 
 	App struct {
-		Version string `validate:"len:5"`
+		Version string `validate:"regexp:\\d+|len:5"`
 	}
 
 	Token struct {
@@ -42,10 +44,84 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "test",
+				Name:   "test",
+				Age:    34,
+				Email:  "test@test.com",
+				Role:   "test",
+				Phones: []string{"12345"},
+				meta:   nil,
+			},
+			expectedErr: fmt.Errorf(
+				"field: ID | err: doesnt fit length\n" +
+					"field: Phones | err: doesnt fit length\n"),
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:     "123456789012345678901234567890123456",
+				Name:   "test",
+				Age:    34,
+				Email:  "test@test.com",
+				Role:   "test",
+				Phones: []string{"12345678901", "88005555555"},
+				meta:   nil,
+			},
+			expectedErr: fmt.Errorf(""),
+		},
+		{
+			in: App{
+				Version: "12323232323",
+			},
+			expectedErr: fmt.Errorf(
+				"field: Version | err: doesnt fit length\n",
+			),
+		},
+		{
+			in: App{
+				Version: "dasda",
+			},
+			expectedErr: fmt.Errorf(
+				"field: Version | err: doesnt fit regexp\n",
+			),
+		},
+		{
+			in: App{
+				Version: "123123dasda",
+			},
+			expectedErr: fmt.Errorf(
+				"field: Version | err: doesnt fit length\n",
+			),
+		},
+		{
+			in: App{
+				Version: "55555",
+			},
+			expectedErr: fmt.Errorf(""),
+		},
+		{
+			in: Token{
+				Header:    []byte{1, 2, 3},
+				Payload:   []byte{4, 5, 6},
+				Signature: []byte{7, 8, 9},
+			},
+			expectedErr: fmt.Errorf(""),
+		},
+		{
+			in: Response{
+				Body: "anytext",
+				Code: 503,
+			},
+			expectedErr: fmt.Errorf(
+				"field: Code | err: doesnt contains in set\n"), //nolint:revive
+		},
+		{
+			in: Response{
+				Body: "anytext",
+				Code: 500,
+			},
+			expectedErr: fmt.Errorf(""),
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +129,7 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			require.EqualError(t, Validate(tt.in), tt.expectedErr.Error())
 		})
 	}
 }
