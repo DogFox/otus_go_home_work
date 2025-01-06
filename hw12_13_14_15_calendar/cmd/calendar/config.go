82cb371e666ad92f64c1267443944f0196938831
Denis Kurov <kurovda@gmail.com>
2025-01-06 09:53:59 +0300

    add viper to parse config

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,50 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
 type Config struct {
-	Logger LoggerConf
-	// TODO
+	Logger   LoggerConf
+	Database DatabaseConf
+	Server   ServerConf
 }
 
 type LoggerConf struct {
 	Level string
-	// TODO
+	File  string
 }
 
-func NewConfig() Config {
-	return Config{}
+type DatabaseConf struct {
+	User     string
+	Password string
+	Name     string
+	Host     string
+	Port     string
 }
 
-// TODO
+type ServerConf struct {
+	Host string
+	Port string
+}
+
+func NewConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../../configs")
+	viper.SetConfigType("yaml")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("не удалось прочитать конфиг: %w", err)
+	}
+	var config Config
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("не удалось распарсить конфиг: %w", err)
+	}
+
+	return &config, nil
+}
